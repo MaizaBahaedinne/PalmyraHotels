96979b29260aa365e2c31cf559aadc11677b4bbe
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-01-01 13:06:29 +0100

    MESSENGER

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -59,6 +59,38 @@
 </head>
 <body>
 
+        <!-- Messenger Plugin de discussion Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Plugin de discussion code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "103054498919121");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    </script>
+
+    <!-- Your SDK code -->
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v12.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+
     <div id="preloader">
         <div class="sk-spinner sk-spinner-wave">
             <div class="sk-rect1"></div>
@@ -69,6 +101,8 @@
         </div>
     </div>
     <!-- End Preload -->
+
+
 
     <div class="layer"></div>
     <!-- Mobile menu overlay mask -->
