00d6b1e5b88075dd083c69290179a8590e87c279
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-01-11 19:06:13 +0100

    bars

--- application/views/bar/view.php
+++ application/views/bar/view.php
@@ -86,6 +86,51 @@
 							</div>
 							<!-- End row  -->
 						</div>
+						<div class="col-md-12">
+							<div id="Img_carousel" class="slider-pro">
+                        <div class="sp-slides">
+
+                             <?php
+                                $dir = "./assets/img/bars/".$bar->acro."/";
+                                $dir1 = base_url()."/assets/img/bars/".$bar->acro."/";
+                              
+
+                                chdir($dir);
+                                array_multisort(array_map('filemtime', ($files = glob("*.{jpg,png,gif}", GLOB_BRACE))), SORT_DESC, $files);
+                                foreach($files as $filename)
+                                {?>
+                                <div class="sp-slide">
+                                    <img alt="Image" class="sp-image" 
+                                    src="<?php echo $dir1.$filename ?>" 
+                                    data-src="<?php echo $dir1.$filename ?>" 
+                                    data-small="<?php echo $dir1.$filename ?>"
+                                    data-medium="<?php echo $dir1.$filename ?>" 
+                                    data-large="<?php echo $dir1.$filename ?>" 
+                                    data-retina="<?php echo $dir1.$filename ?>" >
+
+                                </div>   
+                                <?php } 
+                            ?>
+
+                            
+                            
+
+                          
+                        </div>
+                        
+                        <div class="sp-thumbnails">
+                            <?php
+                                array_multisort(array_map('filemtime', ($files = glob("*.{jpg,png,gif}", GLOB_BRACE))), SORT_DESC, $files);
+                                foreach($files as $filename)
+                                {?>
+                                <img alt="Image" class="sp-thumbnail" src="<?php echo $dir1.$filename ?>" width="40px">   
+                                <?php } 
+                            ?>
+                        </div>
+                        
+                    </div>
+							
+						</div>
 					</div>
 
 					<hr>
@@ -261,50 +306,13 @@
 					<p class="d-none d-xl-block d-lg-block d-xl-none">
 						<a class="btn_map" data-toggle="collapse" href="#collapseMap" aria-expanded="false" aria-controls="collapseMap" data-text-swap="Hide map" data-text-original="View on map">View on map</a>
 					</p>
-					<div class="box_style_1 expose">
-						<h3 class="inner">- Booking -</h3>
-						<div class="row">
-							<div class="col-sm-6">
-								<div class="form-group">
-									<label><i class="icon-calendar-7"></i> Date</label>
-									<input class="date-pick form-control" type="text">
-								</div>
-							</div>
-							<div class="col-sm-6">
-								<div class="form-group">
-									<label><i class=" icon-clock"></i> Time</label>
-									<input class="time-pick form-control" value="12:00 AM" type="text">
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-6">
-								<div class="form-group">
-									<label>Adults</label>
-									<div class="numbers-row">
-										<input type="text" value="1" id="adults" class="qty2 form-control" name="quantity">
-									</div>
-								</div>
-							</div>
-							<div class="col-6">
-								<div class="form-group">
-									<label>Children</label>
-									<div class="numbers-row">
-										<input type="text" value="0" id="children" class="qty2 form-control" name="quantity">
-									</div>
-								</div>
-							</div>
-						</div>
-						<hr>
-						<a class="btn_full" href="payment_restaurant-1.html">Book now</a>
-						<a class="btn_full_outline" href="#"><i class=" icon-heart"></i> Add to whislist</a>
-					</div>
+					
 					<!--/box_style_1 -->
 
 					<div class="box_style_4">
 						<i class="icon_set_1_icon-90"></i>
 						<h4><span>Book</span> by phone</h4>
-						<a href="tel://004542344599" class="phone">+45 423 445 99</a>
+						<a href="tel://+216<?php echo $bar->phone ?>" class="phone">+216 <?php echo $bar->phone ?></a>
 						<small>Monday to Friday 9.00am - 7.30pm</small>
 					</div>
 
