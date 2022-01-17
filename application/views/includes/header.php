0f0bd509d8b9db4d53ca67d09475ef52a89ad50d
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-01-17 21:09:24 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -9,7 +9,7 @@
     <meta property="og:type"               content="website" />
     <meta property="og:title"              content="<?php echo $pageTitle ?>" />
     <meta property="og:description"        content="" />
-    <meta property="og:image"              content="<?php echo base_url() ?>assets/img/facade/hotel/<?php echo $hotel->facade ?>" />
+    <meta property="og:image"              content="<?php echo base_url() ?>assets/img/sitewal.jpg" />
     <meta property="fb:app_id"             content="673400070509276" />
 
 
