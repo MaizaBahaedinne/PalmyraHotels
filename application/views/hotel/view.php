3eeae4997cfdaa6ce897ee1dd79771cd6aeea31c
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-01-21 21:16:52 +0100

    Update view.php

--- application/views/hotel/view.php
+++ application/views/hotel/view.php
@@ -413,7 +413,7 @@
                                 </p>
                                 <div class="rating">
                                     <?php for ($i=0; $i < round($avi->moyenne) ; $i++) { echo '<i class="icon-smile voted"></i>' ;   } ?>
-                                    <?php for ($i=1; $i < 5-round($avi->moyenne) ; $i++) { echo '<i class="icon-smile"></i>' ;   } ?>
+                                    <?php for ($i=0; $i < 5-round($avi->moyenne) ; $i++) { echo '<i class="icon-smile"></i>' ;   } ?>
                                     <?php echo round($avi->moyenne) ?>
                                 </div>
                             </div>
