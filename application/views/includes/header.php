7f301a7b36ceaf56871bbc17fd4c44e2ffaa93b0
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-02-04 13:04:21 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -3,6 +3,87 @@
 
 <head>
 
+     <meta charset="utf-8">
+    <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Palmyra Hotels is one of the world’s most reputable upscale hotel management companies. Since it was established in 2019, manages 2000-plus rooms in more than 4 hotels across Sousse , Nabeul ; Monastir. ">
+    <meta name="author" content="Maiza Bahaedinne">
+    <meta name="keywords" content="Palmyra,Palmyra Hotels,Palmyra Aquapark kantaoui,Palmyra Holiday ressort and spa,Palmyra Club Nabeul,Palmyra Golden beach">
+    <meta name="facebook-domain-verification" content="em8tnon11p3aq8s3e72jtulif3mr5k" />
+
+    
+   
+    <?php if(!empty($type) )
+    { 
+    if ($type=="hotel" && !(empty($hotelDetails)) ) 
+    { 
+    ?>
+    <meta property="og:site_name" content="Palmyra Hotels" />
+    <meta property="og:url"                content="<?php echo base_url() ?>Hotel/view/<?php echo $hotelDetails->hotelId ?>" />
+    <meta property="og:type"               content="website" />
+    <meta property="og:title"              content="Palmyra <?php echo $pageTitle ?> | Palmyra Hotels" />
+    <meta property="og:description"        content="<?php echo $hotelDetails->description ?>" />
+    <meta property="og:image"              content="<?php echo base_url() ?>assets/img/facade/hotel/<?php echo $hotelDetails->facade ?>" />
+    <?php       } 
+
+    if ($type=="bar" && !(empty($barDetails)) ) 
+    { 
+    ?>
+    <meta property="og:site_name" content="Palmyra Hotels" />
+    <meta property="og:url"                content="<?php echo base_url() ?>Bars/view/<?php echo $barDetails->hotelId ?>" />
+    <meta property="og:type"               content="website" />
+    <meta property="og:title"              content="<?php echo $pageTitle ?> | Palmyra Hotels" />
+    <meta property="og:description"        content="<?php echo $barDetails->description ?>" />
+    <meta property="og:image"              content="<?php echo base_url() ?>assets/img/facade/bar/<?php echo $barDetails->repo ?>" />
+    <?php 
+    }
+
+    }else{ ?>
+    <meta property="og:site_name" content="Palmyra Hotels" />
+    <meta property="og:url"                content="<?php echo base_url() ?>" />
+    <meta property="og:type"               content="website" />
+    <meta property="og:title"              content="<?php echo $pageTitle ?> | Palmyra Hotels" />
+    <meta property="og:description"        content="Palmyra Hotels is one of the world’s most reputable upscale hotel management companies. Since it was established in 2019, manages 2000-plus rooms in more than 4 hotels across Sousse , Nabeul ; Monastir." />
+    <meta property="og:image"              content="<?php echo base_url() ?>assets/img/sitewal.jpg" />   
+    <?php  } ?>  
+
+
+    <meta property="fb:app_id"             content="673400070509276" />
+
+
+
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-KP7L2TPBWD"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-KP7L2TPBWD');
+    </script>
+
+
+    <title>Palmyra Hotels | <?php echo $pageTitle ?></title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="<?php echo base_url() ?>assets/img/favicon-1.ico" type="image/x-icon">
+
+
+    <!-- GOOGLE WEB FONT -->
+    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Google+Sans_old:400,500,700|Google+Sans+Text:400">
+
+    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Google+Sans+Text:400&amp;text=%E2%86%90%E2%86%92%E2%86%91%E2%86%93">
+
+    <style>.gm-ui-hover-effect{opacity:.6}.gm-ui-hover-effect:hover{opacity:1}
+    </style>
+    <style>.gm-style .gm-style-cc a,.gm-style .gm-style-cc button,.gm-style .gm-style-cc span,.gm-style .gm-style-mtc div{font-size:10px;box-sizing:border-box}.gm-style .gm-style-cc a,.gm-style .gm-style-cc button,.gm-style .gm-style-cc span{outline-offset:3px}
+    </style>
+    <style>@media print {  .gm-style .gmnoprint, .gmnoprint {    display:none  }}@media screen {  .gm-style .gmnoscreen, .gmnoscreen {    display:none  }}</style>
+    <style>.gm-style-moc{background-color:rgba(0,0,0,0.45);pointer-events:none;text-align:center;transition:opacity ease-in-out}.gm-style-mot{color:white;font-family:Roboto,Arial,sans-serif;font-size:22px;margin:0;position:relative;top:50%;transform:translateY(-50%);-webkit-transform:translateY(-50%);-ms-transform:translateY(-50%)}
+    </style>
+    <style>.gm-style img{max-width: none;}.gm-style {font: 400 11px Roboto, Arial, sans-serif; text-decoration: none;}</style>
     <style type="text/css">
        .mobileShow { display: none;}
        /* Smartphone Portrait and Landscape */
@@ -102,81 +183,7 @@
 
 
 
-</style>
-   
-    <?php if(!empty($type) )
-    { 
-    if ($type=="hotel" && !(empty($hotelDetails)) ) 
-    { 
-    ?>
-    <meta property="og:url"                content="<?php echo base_url() ?>Hotel/view/<?php echo $hotelDetails->hotelId ?>" />
-    <meta property="og:type"               content="website" />
-    <meta property="og:title"              content="Palmyra <?php echo $pageTitle ?> | Palmyra Hotels" />
-    <meta property="og:description"        content="<?php echo $hotelDetails->description ?>" />
-    <meta property="og:image"              content="<?php echo base_url() ?>assets/img/facade/hotel/<?php echo $hotelDetails->facade ?>" />
-    <?php       } 
-
-    if ($type=="bar" && !(empty($barDetails)) ) 
-    { 
-    ?>
-    <meta property="og:url"                content="<?php echo base_url() ?>Bars/view/<?php echo $barDetails->hotelId ?>" />
-    <meta property="og:type"               content="website" />
-    <meta property="og:title"              content="<?php echo $pageTitle ?> | Palmyra Hotels" />
-    <meta property="og:description"        content="<?php echo $barDetails->description ?>" />
-    <meta property="og:image"              content="<?php echo base_url() ?>assets/img/facade/bar/<?php echo $barDetails->repo ?>" />
-    <?php 
-    }
-
-    }else{ ?>
-    <meta property="og:url"                content="<?php echo base_url() ?>" />
-    <meta property="og:type"               content="website" />
-    <meta property="og:title"              content="<?php echo $pageTitle ?> | Palmyra Hotels" />
-    <meta property="og:description"        content="Palmyra Hotels is one of the world’s most reputable upscale hotel management companies. Since it was established in 2019, manages 2000-plus rooms in more than 4 hotels across Sousse , Nabeul ; Monastir." />
-    <meta property="og:image"              content="<?php echo base_url() ?>assets/img/sitewal.jpg" />   
-    <?php  } ?>  
-
-
-    <meta property="fb:app_id"             content="673400070509276" />
-
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Palmyra Hotels ">
-    <meta name="author" content="Maiza Bahaedinne">
-    <meta name="facebook-domain-verification" content="em8tnon11p3aq8s3e72jtulif3mr5k" />
-
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-KP7L2TPBWD"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-KP7L2TPBWD');
-    </script>
-
-
-    <title>Palmyra Hotels | <?php echo $pageTitle ?></title>
-
-    <!-- Favicons-->
-    <link rel="shortcut icon" href="<?php echo base_url() ?>assets/img/favicon-1.ico" type="image/x-icon">
-
-
-    <!-- GOOGLE WEB FONT -->
-    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Google+Sans_old:400,500,700|Google+Sans+Text:400">
-
-    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Google+Sans+Text:400&amp;text=%E2%86%90%E2%86%92%E2%86%91%E2%86%93">
-
-    <style>.gm-ui-hover-effect{opacity:.6}.gm-ui-hover-effect:hover{opacity:1}
     </style>
-    <style>.gm-style .gm-style-cc a,.gm-style .gm-style-cc button,.gm-style .gm-style-cc span,.gm-style .gm-style-mtc div{font-size:10px;box-sizing:border-box}.gm-style .gm-style-cc a,.gm-style .gm-style-cc button,.gm-style .gm-style-cc span{outline-offset:3px}
-    </style>
-    <style>@media print {  .gm-style .gmnoprint, .gmnoprint {    display:none  }}@media screen {  .gm-style .gmnoscreen, .gmnoscreen {    display:none  }}</style>
-    <style>.gm-style-moc{background-color:rgba(0,0,0,0.45);pointer-events:none;text-align:center;transition:opacity ease-in-out}.gm-style-mot{color:white;font-family:Roboto,Arial,sans-serif;font-size:22px;margin:0;position:relative;top:50%;transform:translateY(-50%);-webkit-transform:translateY(-50%);-ms-transform:translateY(-50%)}
-    </style>
-    <style>.gm-style img{max-width: none;}.gm-style {font: 400 11px Roboto, Arial, sans-serif; text-decoration: none;}</style>
-
 
 
     <!-- ALTERNATIVE COLORS CSS -->
@@ -190,9 +197,6 @@
 
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.3.10/sweetalert2.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.3.10/sweetalert2.min.js"></script>
-  
-
-        
   
 </head>
 <body>
