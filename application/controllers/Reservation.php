90ea50b3af8dfd31f4ddcaad99b91a0effc20715
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-07-13 22:20:59 +0100

    r

--- application/controllers/Reservation.php
+++ application/controllers/Reservation.php
@@ -13,133 +13,7 @@ class Reservation extends BaseController
 {
 
 
-    $this->sig = 'Best regards , Cordialement , أطيب التحيات ,
-                <table cellpadding="0" cellspacing="0" class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;">
-                   <tbody>
-                      <tr>
-                         <td>
-                            <table cellpadding="0" cellspacing="0" class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;">
-                               <tbody>
-                                  <tr>
-                                     <td style="vertical-align: top;">
-                                        <table cellpadding="0" cellspacing="0" class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;">
-                                           <tbody>
-                                              <tr>
-                                                 <td class="sc-TOsTZ kjYrri" style="text-align: center;"><img src="https://palmyrahotels.tn/assets/img/logopage.png" role="presentation" width="130" class="sc-cHGsZl bHiaRe" style="max-width: 130px; display: block;"></td>
-                                              </tr>
-                                              <tr>
-                                                 <td height="30"></td>
-                                              </tr>
-                                              <tr>
-                                                 <td style="text-align: center;">
-                                                    <table cellpadding="0" cellspacing="0" class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial; display: inline-block;">
-                                                       <tbody>
-                                                          <tr style="text-align: center;">
-                                                             <td><a href="https://www.facebook.com/palmyrahotels.tn" color="#F0B942" class="sc-hzDkRC kpsoyz" style="display: inline-block; padding: 0px; background-color: rgb(240, 185, 66);"><img src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/facebook-icon-2x.png" alt="facebook" color="#F0B942" height="24" class="sc-bRBYWo ccSRck" style="background-color: rgb(240, 185, 66); max-width: 135px; display: block;"></a></td>
-                                                             <td width="5">
-                                                                <div></div>
-                                                             </td>
-                                                             <td><a href="https://www.instagram.com/palmyrahotels.tn/" color="#F0B942" class="sc-hzDkRC kpsoyz" style="display: inline-block; padding: 0px; background-color: rgb(240, 185, 66);"><img src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/instagram-icon-2x.png" alt="instagram" color="#F0B942" height="24" class="sc-bRBYWo ccSRck" style="background-color: rgb(240, 185, 66); max-width: 135px; display: block;"></a></td>
-                                                             <td width="5">
-                                                                <div></div>
-                                                             </td>
-                                                          </tr>
-                                                       </tbody>
-                                                    </table>
-                                                 </td>
-                                              </tr>
-                                           </tbody>
-                                        </table>
-                                     </td>
-                                     <td width="46">
-                                        <div></div>
-                                     </td>
-                                     <td style="padding: 0px; vertical-align: middle;">
-                                        <h3 color="#000000" class="sc-fBuWsC eeihxG" style="margin: 0px; font-size: 18px; color: rgb(0, 0, 0);"><span>Palmyra</span><span>&nbsp;</span><span>Hotels</span></h3>
-                                        <p color="#000000" font-size="medium" class="sc-fMiknA bxZCMx" style="margin: 0px; color: rgb(0, 0, 0); font-size: 14px; line-height: 22px;"><span>Responsable Marketing digital</span></p>
-                                        <p color="#000000" font-size="medium" class="sc-dVhcbM fghLuF" style="margin: 0px; font-weight: 500; color: rgb(0, 0, 0); font-size: 14px; line-height: 22px;"><span>Marketing</span><span>&nbsp;|&nbsp;</span><span>Palmyra Hotels</span></p>
-                                        <table cellpadding="0" cellspacing="0" class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial; width: 100%;">
-                                           <tbody>
-                                              <tr>
-                                                 <td height="30"></td>
-                                              </tr>
-                                              <tr>
-                                                 <td color="#f0b942" direction="horizontal" height="1" class="sc-jhAzac hmXDXQ" style="width: 100%; border-bottom: 1px solid rgb(240, 185, 66); border-left: none; display: block;"></td>
-                                              </tr>
-                                              <tr>
-                                                 <td height="30"></td>
-                                              </tr>
-                                           </tbody>
-                                        </table>
-                                        <table cellpadding="0" cellspacing="0" class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;">
-                                           <tbody>
-                                              <tr height="25" style="vertical-align: middle;">
-                                                 <td width="30" style="vertical-align: middle;">
-                                                    <table cellpadding="0" cellspacing="0" class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;">
-                                                       <tbody>
-                                                          <tr>
-                                                             <td style="vertical-align: bottom;"><span color="#f0b942" width="11" class="sc-jlyJG bbyJzT" style="display: block; background-color: rgb(240, 185, 66);"><img src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/phone-icon-2x.png" color="#f0b942" width="13" class="sc-iRbamj blSEcj" style="display: block; background-color: rgb(240, 185, 66);"></span></td>
-                                                          </tr>
-                                                       </tbody>
-                                                    </table>
-                                                 </td>
-                                                 <td style="padding: 0px; color: rgb(0, 0, 0);"></td>
-                                              </tr>
-                                              <tr height="25" style="vertical-align: middle;">
-                                                 <td width="30" style="vertical-align: middle;">
-                                                    <table cellpadding="0" cellspacing="0" class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;">
-                                                       <tbody>
-                                                          <tr>
-                                                             <td style="vertical-align: bottom;"><span color="#f0b942" width="11" class="sc-jlyJG bbyJzT" style="display: block; background-color: rgb(240, 185, 66);"><img src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/email-icon-2x.png" color="#f0b942" width="13" class="sc-iRbamj blSEcj" style="display: block; background-color: rgb(240, 185, 66);"></span></td>
-                                                          </tr>
-                                                       </tbody>
-                                                    </table>
-                                                 </td>
-                                                 <td style="padding: 0px;"><a href="mailto:contact@palmyrahotels.tn" color="#000000" class="sc-gipzik iyhjGb" style="text-decoration: none; color: rgb(0, 0, 0); font-size: 12px;"><span>contact@palmyrahotels.tn</span></a></td>
-                                              </tr>
-                                              <tr height="25" style="vertical-align: middle;">
-                                                 <td width="30" style="vertical-align: middle;">
-                                                    <table cellpadding="0" cellspacing="0" class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;">
-                                                       <tbody>
-                                                          <tr>
-                                                             <td style="vertical-align: bottom;"><span color="#f0b942" width="11" class="sc-jlyJG bbyJzT" style="display: block; background-color: rgb(240, 185, 66);"><img src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/link-icon-2x.png" color="#f0b942" width="13" class="sc-iRbamj blSEcj" style="display: block; background-color: rgb(240, 185, 66);"></span></td>
-                                                          </tr>
-                                                       </tbody>
-                                                    </table>
-                                                 </td>
-                                                 <td style="padding: 0px;"><a href="//www.palmyrahotels.tn" color="#000000" class="sc-gipzik iyhjGb" style="text-decoration: none; color: rgb(0, 0, 0); font-size: 12px;"><span>www.palmyrahotels.tn</span></a></td>
-                                              </tr>
-                                              <tr height="25" style="vertical-align: middle;">
-                                                 <td width="30" style="vertical-align: middle;">
-                                                    <table cellpadding="0" cellspacing="0" class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;">
-                                                       <tbody>
-                                                          <tr>
-                                                             <td style="vertical-align: bottom;"><span color="#f0b942" width="11" class="sc-jlyJG bbyJzT" style="display: block; background-color: rgb(240, 185, 66);"><img src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/address-icon-2x.png" color="#f0b942" width="13" class="sc-iRbamj blSEcj" style="display: block; background-color: rgb(240, 185, 66);"></span></td>
-                                                          </tr>
-                                                       </tbody>
-                                                    </table>
-                                                 </td>
-                                                 <td style="padding: 0px;"><span color="#000000" class="sc-csuQGl CQhxV" style="font-size: 12px; color: rgb(0, 0, 0);"><span>Blue star Hotels, route touristique kantaoui, Sousse</span></span></td>
-                                              </tr>
-                                           </tbody>
-                                        </table>
-                                        <table cellpadding="0" cellspacing="0" class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;">
-                                           <tbody>
-                                              <tr>
-                                                 <td height="30"></td>
-                                              </tr>
-                                           </tbody>
-                                        </table>
-                                     </td>
-                                  </tr>
-                               </tbody>
-                            </table>
-                         </td>
-                      </tr>
-                   </tbody>
-                </table>
-
-                <img  src="https://www.palmyrahotels.tn/assets/img/private/ban.jpg"> ' ;
+    
 
     /**
      * This is default constructor of the class
@@ -293,7 +167,7 @@ class Reservation extends BaseController
                 
                 
                 <br>
-                ".$sig ;
+                ".$this->global['sig'] ;
 
                if( $this->send_mail(
                     $data['reservation']->client->email  , 
@@ -316,7 +190,7 @@ class Reservation extends BaseController
                 
                
                 <br>
-                ".$sig ;
+                ". $this->global['sig'] ;
 
                         $this->send_mail(
                     $data['reservation']->hotel->mailresa  , 
